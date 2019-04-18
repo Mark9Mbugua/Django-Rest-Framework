71d70ce54ee12e00876459f61234e595bc3e3379
Mark Mbugua <mbuguamark@gmail.com>
2019-04-18 18:33:45 +0300

    Create API

--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Hotel
+from .serializers import HotelSerializer
 
-# Create your views here.
+
+class HotelView(viewsets.ModelViewSet):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+
+
